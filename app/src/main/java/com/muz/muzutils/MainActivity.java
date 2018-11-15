fcb333583c54b2c558028f0497e85b1174f8d149
strayer721 <1248562370@qq.com>
2018-11-15 20:13:48 +0800

    learn snackbar

--- app/src/main/java/com/muz/muzutils/MainActivity.java
+++ app/src/main/java/com/muz/muzutils/MainActivity.java
@@ -1,12 +1,68 @@
 package com.muz.muzutils;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.muz.muzutils.snackbar.MuzBaseTransientBottomBar;
+import com.muz.muzutils.snackbar.MuzSnackbar;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.main_const);
+        Snackbar snackbars = Snackbar.make(constraintLayout, "幕栀", Snackbar.LENGTH_LONG);
+        MuzSnackbar snackbar = MuzSnackbar.make(constraintLayout, "幕栀", MuzSnackbar.LENGTH_SHORT);
+        snackbar.setAction("你好！", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "你好！", Toast.LENGTH_SHORT).show();
+            }
+        });
+        snackbar.setActionTextColor(0xFF3F51B5);
+        snackbar.setText("不好");
+
+        snackbar.addCallback(new MuzBaseTransientBottomBar.BaseCallback<MuzSnackbar>() {
+            @Override
+            public void onDismissed(MuzSnackbar transientBottomBar, int event) {
+                super.onDismissed(transientBottomBar, event);
+                switch (event) {
+                    case MuzSnackbar.Callback.DISMISS_EVENT_CONSECUTIVE:
+                    case MuzSnackbar.Callback.DISMISS_EVENT_MANUAL:
+                    case MuzSnackbar.Callback.DISMISS_EVENT_SWIPE:
+                    case MuzSnackbar.Callback.DISMISS_EVENT_TIMEOUT:
+                        Toast.makeText(MainActivity.this, "删除成功", Toast.LENGTH_SHORT).show();
+                        break;
+                    case MuzSnackbar.Callback.DISMISS_EVENT_ACTION:
+                        Toast.makeText(MainActivity.this, "撤销了删除操作", Toast.LENGTH_SHORT).show();
+                        break;
+
+                }
+                Log.e("MainActivity", "onDismissed");
+            }
+
+            @Override
+            public void onShown(MuzSnackbar transientBottomBar) {
+                super.onShown(transientBottomBar);
+                Log.e("MainActivity", "onShown");
+            }
+        });
+//        snackbar.setDuration(5000);
+        snackbar.getView().setBackgroundResource(R.drawable.ic_launcher_background);
+        //snackbar.getView().setEnabled(false);毫无意义，并且没有效果
+        snackbar.getView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "外部点击！", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        snackbar.getView().getTag(1);
+//        snackbar.getView()
+        snackbar.show();
     }
 }
